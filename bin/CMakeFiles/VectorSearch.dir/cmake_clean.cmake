b8473d9ac03c5e4f0ffaec5d3fa5c4268e47af00
vir <virpaksh.ai@gmail.com>
2025-07-13 21:10:40 +0530

    Initial commit

---
+++ bin/CMakeFiles/VectorSearch.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VectorSearch.dir/main.cpp.o"
+  "CMakeFiles/VectorSearch.dir/main.cpp.o.d"
+  "CMakeFiles/VectorSearch.dir/vector_search.cpp.o"
+  "CMakeFiles/VectorSearch.dir/vector_search.cpp.o.d"
+  "VectorSearch"
+  "VectorSearch.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VectorSearch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
